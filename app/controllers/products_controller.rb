4f49fb08a42f80012eeac82bc3334e1872fc892a
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-05-25 17:48:52 +0200

    footer, index, show, new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,34 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
   def index
     @products = Product.all
   end
 
+  def show
+  end
+
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to "/products", notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name)
   end
 end
