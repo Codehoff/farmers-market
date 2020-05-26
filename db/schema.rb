b85256b42597e005e49bbc7b16adbcfea55664e6
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-05-26 17:52:57 +0200

    whatever

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2020_05_26_130604) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
