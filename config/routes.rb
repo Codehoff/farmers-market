546486727bc139b15776b479dcd384d84e6ac992
Felix Meumann <f.meumann@gmail.com>
2020-05-26 17:44:36 +0200

    added the order model, controller, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :products, only: [:index, :show, :new, :create]
+  resources :products, only: [:index, :show, :new, :create] do
+    resources :orders, only: [:new, :create]
+  end
+  resources :orders, only: [:index]
+
+  #shall we also allow for a :destroy resource with orders??
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
