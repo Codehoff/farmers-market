82e905a007a229b47f9314fe7ac725cf76b01a04
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-05-26 14:50:01 +0200

    product backend + start of cloudinary

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+    belongs_to :user
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+    validates :category, presence: true
+    validates :stock_info, presence: true
+    validates :unit, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,15 @@ ActiveRecord::Schema.define(version: 2020_05_25_142550) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
+    t.string "category"
+    t.string "price"
+    t.string "description"
+    t.integer "stock_info"
+    t.string "unit"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_05_25_142550) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
