5d2d0c106a5773e8d56ddb449ceb378b68abcb48
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-05-28 17:00:34 +0200

    form now with name,address,city

--- db/migrate/20200525125132_devise_create_users.rb
+++ db/migrate/20200525125132_devise_create_users.rb
@@ -5,6 +5,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :first_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
+      t.string :address, null: false, default: ""
+      t.string :postal_code, null: false, default: ""
+      t.string :city, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -37,6 +42,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :first_name                
+    add_index :users, :last_name                
+    add_index :users, :address
+    add_index :users, :postal_code
+    add_index :users, :city
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
