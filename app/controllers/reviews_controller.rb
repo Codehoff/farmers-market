0af2cbf7b961c78d7660d57da2ebbf47b73775e7
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-05-28 13:09:52 +0200

    order view/verified-reviews/seller-buyer-connection

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,7 +17,8 @@ class ReviewsController < ApplicationController
   def create
       @product = Product.find(params[:product_id])
       @review = Review.new(review_params)
-      @review.product = @product     
+      @review.product = @product
+      @review.user_id = current_user.id  
       if @review.save
           redirect_to product_path(@product)
       else
