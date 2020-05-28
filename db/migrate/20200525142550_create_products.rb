416064637ed6d359cb293fccfab6ac59d3302adc
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-05-28 13:44:31 +0200

    some more order index stuff

--- db/migrate/20200525142550_create_products.rb
+++ db/migrate/20200525142550_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string :name
       t.string :category
-      t.string :price
+      t.integer :price
       t.string :description
       t.integer :stock_info
       t.string :unit
